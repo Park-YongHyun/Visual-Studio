d8c71ab002fed2e423b3402777e198bc23a06c96
tmp20190820 <54312936+Park-YongHyun@users.noreply.github.com>
2021-07-31 18:10:51 +0900

    v20210731

---
+++ Solution1/UnmuteSystemSounds/Program.cs
@@ -0,0 +1,27 @@
+﻿using CSCore.CoreAudioAPI; // https://github.com/filoe/cscore
+
+namespace UnmuteSystemSounds
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using var deviceEnumerator = new MMDeviceEnumerator();
+			using var devices = deviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+			foreach (var device in devices)
+			{
+				using var sessionManager = AudioSessionManager2.FromMMDevice(device);
+				using var sessionEnumerator = sessionManager.GetSessionEnumerator();
+				foreach (var session in sessionEnumerator)
+				{
+					using var sessionControl = session.QueryInterface<AudioSessionControl2>();
+					if (sessionControl.IsSystemSoundSession == true)
+					{
+						using var sessionVolume = session.QueryInterface<SimpleAudioVolume>();
+						sessionVolume.IsMuted = false;
+					}
+				}
+			}
+		}
+	}
+}
